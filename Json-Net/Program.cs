7643a75e1f267c9857d2c1bf320cc8fe515e820d
UchiTesing <uchitesting57@gmail.com>
2023-06-18 22:45:44 +0200

    System.Web.Json.Helpers.Json.Decode equivalent in .NET 6
    Fix JSON sln file

---
+++ Json-Net/Program.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json.Linq;
+
+#region Path Setup
+string fileName = "file.json";
+string baseDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory);
+string baseDirJson = Path.Combine(Directory.GetParent(baseDir)
+	.Parent.Parent.Parent.Parent.FullName,fileName);
+
+Console.WriteLine($"{baseDir}{Environment.NewLine}{baseDirJson}");
+#endregion
+
+#region JSON Setup
+var jsonTextData = File.ReadAllText(baseDirJson);
+JObject o = JObject.Parse(jsonTextData);
+
+var team = o["team"];
+#endregion
+
+#region Raw JToken
+Console.WriteLine("\nRaw JToken\n==========\n");
+
+foreach (JToken item in team)
+{
+	// Keys are case sensitive
+	Console.WriteLine(item["id"]);
+	Console.WriteLine(item["Name"]);
+	Console.WriteLine(item["Age"]);
+}
+#endregion
+
+#region Using LINQ
+Console.WriteLine("\nUsing LINQ\n==========\n");
+
+team.Select((item) =>
+	new Person
+	{
+		Id = Convert.ToInt32(item["id"]),
+		Name = item["Name"].ToString(),
+		Age = Convert.ToInt32(item["Age"])
+	}
+).ToList().ForEach(item =>
+{
+	Console.WriteLine(item.Id);
+	Console.WriteLine(item.Name);
+	Console.WriteLine(item.Age);
+});
+#endregion
---
+++ Json-NetF/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UT_Concepts
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var path = @"D:\Users\UchiTesting\Desktop\repos\C#\UT-Concepts\file.json";
+			dynamic decoded = System.Web.Helpers.Json.Decode(File.ReadAllText(path));
+
+			foreach (var item in decoded.Team)
+			{
+				Console.WriteLine(item.id);
+				Console.WriteLine(item.Name);
+				Console.WriteLine(item.Age);
+			}
+		}
+	}
+}
